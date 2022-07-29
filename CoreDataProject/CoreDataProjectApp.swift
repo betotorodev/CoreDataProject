fa365a292da9764e0bb3594e7de147ae62c56a5c
Beto Toro <beto.toro@zemoga.com>
2022-07-29 09:53:53 -0500

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Beto Toro on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
