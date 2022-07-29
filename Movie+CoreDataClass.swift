3fdd8c41c629efb519a67b669525ab8e17bd3315
Beto Toro <betotoro0902@gmail.com>
2022-07-29 10:44:35 -0500

    [feat]: NSManageObject subclasses class

---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Beto Toro on 29/07/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
---
+++ Movie+CoreDataProperties.swift
@@ -0,0 +1,32 @@
+//
+//  Movie+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Beto Toro on 29/07/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Movie {
+  
+  @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+    return NSFetchRequest<Movie>(entityName: "Movie")
+  }
+  
+  @NSManaged public var title: String?
+  @NSManaged public var director: String?
+  @NSManaged public var year: Int16
+  
+  public var wrappedTitle: String {
+    title ?? "Unknown Title"
+  }
+  
+  
+}
+
+extension Movie : Identifiable {
+  
+}
