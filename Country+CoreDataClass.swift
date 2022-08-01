a79211aebd3f8992b12ae4a24b3d6cc8a1f203fb
Beto Toro <betotoro0902@gmail.com>
2022-08-01 16:32:15 -0500

    [feat]: relations in core data class

---
+++ Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Beto Toro on 1/08/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
